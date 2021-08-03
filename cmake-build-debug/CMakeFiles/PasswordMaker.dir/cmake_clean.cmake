dbb5b293307e2897d98cbc9e07f810406f5fe087
BuildTools <unconfigured@null.spigotmc.org>
2021-08-03 16:54:46 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/PasswordMaker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PasswordMaker.dir/main.c.o"
+  "PasswordMaker"
+  "PasswordMaker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PasswordMaker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/PasswordMaker.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PasswordMaker.
+# This may be replaced when dependencies are built.
