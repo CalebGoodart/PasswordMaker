7c5c3dfdb30f2811efa55b26b879a74a30793b65
BuildTools <unconfigured@null.spigotmc.org>
2021-08-03 19:05:22 -0500

    added user menu.

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int cases, symbol, nums, random;
+    int length;
+
+    printf("Password Options: 1 for yes, 0 for no\n"
+           "must have upper & lower case\n");
+    scanf(" %d", &cases);
+    printf("must have symbols\n");
+    scanf(" %d", &symbol);
+    printf("must have numbers\n");
+    scanf(" %d", &nums);
+    printf("Random Password?\n");
+    scanf(" %d", &random);
+    printf("Enter a length for password between 0 and 20");
+    scanf(" %d", &length);
+    printf("%d, %d, %d, %d, %d", cases, symbol, nums, random, length);
+
+
+
     return 0;
 }
