57bae548686458d5deb1424227b56fb9c9e29782
BuildTools <unconfigured@null.spigotmc.org>
2021-08-09 16:31:01 -0500

    add password maker and minor menu changes

--- main.c
+++ main.c
@@ -1,23 +1,55 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    int cases, symbol, nums, random;
+    int cases, symbol, nums;
     int length;
+    char symb[] = "!@#$%^&*()_+=-";
+    char lower[] = "abcdefghijklmnopqrstuvwxyz";
+    char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char numbers[] = "1234567890";
 
     printf("Password Options: 1 for yes, 0 for no\n"
-           "must have upper & lower case\n");
+           "can have upper & lower case\n");
     scanf(" %d", &cases);
-    printf("must have symbols\n");
+    printf("can have symbols\n");
     scanf(" %d", &symbol);
-    printf("must have numbers\n");
+    printf("can have numbers\n");
     scanf(" %d", &nums);
-    printf("Random Password?\n");
-    scanf(" %d", &random);
-    printf("Enter a length for password between 0 and 20");
+    printf("Enter a length for password between 1 and 20\n");
     scanf(" %d", &length);
-    printf("%d, %d, %d, %d, %d", cases, symbol, nums, random, length);
+    printf("%d, %d, %d, %d\n", cases, symbol, nums, length);
 
+    char selection[77];
 
+    if (cases) {
+        strcat(selection, lower);
+    }
 
+    if (symbol) {
+        strcat(selection, symb);
+    }
+
+    if (nums) {
+        strcat(selection, numbers);
+    }
+
+    strcat(selection, upper);
+
+    srand(time(0));
+    char password[length];
+    password[length] = '\0';
+    int randlen = strlen(selection);
+    for (int i = 0; i < length; ++i) {
+        char temp = selection[rand() % randlen];
+        password[i] = temp;
+    }
+
+    printf("%s\n", password);
     return 0;
 }
+//FAdhtNCmrRYn
+//FAdhtNCmrRYnsU
+//E8iMLZ5Zuz4h�U
